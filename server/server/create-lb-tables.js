423d6c2fa936be271128aafba17cb318884cf6af
Ben Booth <bkbooth@gmail.com>
2017-08-05 13:57:36 +1000

    Use Promises instead of callbacks

--- server/server/create-lb-tables.js
+++ server/server/create-lb-tables.js
@@ -3,9 +3,7 @@
 const server = require('./server')
 const ds = server.dataSources.db
 const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role']
-ds.automigrate(lbTables, (err) => {
-  if (err) throw err
 
-  console.log(`Loopback tables [${lbTables.join(', ')}] created in ${ds.adapter.name}`)
-  ds.disconnect()
-})
+ds.automigrate(lbTables)
+  .then(() => console.log(`Loopback tables [${lbTables.join(', ')}] created in ${ds.adapter.name}`))
+  .then(() => ds.disconnect())
