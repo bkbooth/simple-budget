f67688cfd4efe570d91c2f41fd078eb86f357b1e
Ben Booth <bkbooth@gmail.com>
2017-08-07 22:36:09 +1000

    Redirect / API requests to /up

--- server/server/boot/root.js
+++ server/server/boot/root.js
@@ -4,5 +4,6 @@ module.exports = function(server) {
   // Install a `/up` route that returns server status
   const router = server.loopback.Router()
   router.get('/up', server.loopback.status())
+  router.get('/', (req, res) => res.redirect('/up'))
   server.use(router)
 }
