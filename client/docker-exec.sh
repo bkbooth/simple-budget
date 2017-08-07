c066ec99b83afea8e474354782a45092bf4fe242
Ben Booth <bkbooth@gmail.com>
2017-08-07 22:25:10 +1000

    Use node v6 and yarn for client
    Use node:slim as the base image

--- client/docker-exec.sh
+++ client/docker-exec.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONTAINER="node:6"
+CONTAINER="node:6-slim"
 
 # Always pull latest image
 docker pull $CONTAINER
@@ -10,4 +10,4 @@ docker run \
   --name=npm-$(( $RANDOM % 99999 )) \
   -v $(pwd):/app \
   -w "/app" \
-  $CONTAINER npm --quiet ${*}
+  $CONTAINER ${*}
